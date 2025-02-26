48d95297cd05dcbaf8d6f709fa5474e71c88a527
Matias Tomas Svec <tomassvec0@gmail.com>
2025-02-26 10:58:40 -0300

    D

---
+++ dist/KioscoFrontend/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-63LYYAW7.mjs";import"./chunk-7CLDAD4R.mjs";import"./chunk-5QU7XVY4.mjs";import"./chunk-J4BDXTMW.mjs";import"./chunk-NG6GRI4F.mjs";import"./chunk-RUC5EUIT.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
